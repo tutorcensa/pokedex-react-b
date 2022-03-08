3db8f43f0de2ca8fb925f059cfd4e17d8ec1ae11
Daniel F. Rivera C <tutormanizales17@censa.edu.co>
2022-03-07 20:17:52 -0500

    Terminar de organizar archivos mínimos del proyecto

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    <h1>Hola Amigos</h1>,
-    document.getElementById('root')
+    <h1>Hola Mundo!</h1>,
+    document.getElementById('app')
 );

23c4c9bba802d8a0492382d9cd5e49ff854e7cff
Daniel F. Rivera C <tutormanizales17@censa.edu.co>
2022-03-07 20:53:53 -0500

    Guardar parcialmente los cambios

---
+++ src/components/pages/home/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DefaultLayout from '../../templates/default-layout/DefaultLayout';
+
+export const Home = () => {
+  return <DefaultLayout />;
+};
+
+export default Home;
